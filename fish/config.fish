aaf0b95b1f5c281d203f5fd930582cb18b7cc483
Ricardo A. Sanjur G. <ricardosanjurg@gmail.com>
2024-08-03 22:54:36 -0500

    configuracion del shell fish

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+set -g fish_greeting
+alias cdd 'cd /mnt/c/Users/ricar/'
